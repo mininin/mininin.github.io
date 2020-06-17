71b43921bb7707f6962d65e67c7bbf111d600115
mininin <wlals.ictis@google.com>
2020-06-17 16:02:01 +0900

    test

--- js/gretting.js
+++ js/gretting.js
@@ -1,4 +1,4 @@
-// class 및 input 받아옴
+// 1. class 및 input 받아옴
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
@@ -7,10 +7,14 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+// value 저장
 function saveName(text) {
     localStorage.setItem(USER_LS, text);
 }
 
+// input에 기존 html밖으로 데이터를 보내버리는 이벤트를 삭제?
+// currentValue에 input.value input에 들어가있는 값을 저장
+// paintGreeting 함수와 saveName함수를 다 실행한다.
 function handleSubmit(event) {
     event.preventDefault();
     const currentValue = input.value;
@@ -30,6 +34,7 @@ function paintGreeting(text) {
     greeting.innerText = `Hello ${text}`;
 }
 
+// 1
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     // localStorage.getItem("key") = key에 대한 value값을 currentUser에 넣음.
