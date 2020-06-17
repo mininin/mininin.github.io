bd206d9065d9fb4aa0273feb0e162d1b90d72976
mininin <wlals.ictis@google.com>
2020-06-17 12:35:05 +0900

    test

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,4 @@
-const ftoDoForm = document.querySelector(".js-toDoForm"),
+const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput = form.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
